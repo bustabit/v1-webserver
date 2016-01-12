53d3229d0f142fdc789485fe8f40d08d767284e5
Ryan Havar <ryan@moneypot.com>
2016-01-12 14:28:58 +0000

    Quick language changes

--- server/sendEmail.js
+++ server/sendEmail.js
@@ -28,7 +28,7 @@ exports.contact = function(from, content, user, callback) {
         to: config.CONTACT_EMAIL,
         from: 'contact@moneypot.com',
         replyTo: from,
-        subject: 'Moneypot Contact (' + from + ')',
+        subject: 'Bustabit Contact (' + from + ')',
         html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
             '<html xmlns="http://www.w3.org/1999/xhtml">' +
             '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' +
